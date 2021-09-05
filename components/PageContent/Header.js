f56264401d266de4e76eda5ae8cf6276c133a9ba
Divine Orji <dpkreativ@gmail.com>
2021-09-05 22:09:23 +0100

    put header component in Header folder

--- components/PageContent/Header.js
+++
@@ -1,5 +0,0 @@
-const Header = () => {
-  return <header>This is the header</header>;
-};
-
-export default Header;
---
+++ components/PageContent/Header/index.js
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <header className="p-5 flex">This is the header</header>;
+};
+
+export default Header;
