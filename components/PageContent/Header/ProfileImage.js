eaca595b44cad88d6509b3484982e81ca4213b0c
Divine Orji <dpkreativ@gmail.com>
2021-09-05 22:34:07 +0100

    properly set up image component from nextjs

--- components/PageContent/Header/ProfileImage.js
+++ components/PageContent/Header/ProfileImage.js
@@ -1,19 +1,18 @@
 import Image from "next/image";
 
-const ProfileImage = ({ className }) => {
+const ProfileImage = () => {
   return (
-    <div className={className}>
-      <div className="w-20 h-20 md:w-40 md:h-40 mx-auto">
-        {/* <img
-          src="https://res.cloudinary.com/dpkreativ/image/upload/v1616357794/IMG_-oi62yk_wtfop2.jpg"
-          alt="main profile"
-          className="rounded-full w-full h-full"
-        /> */}
+    <div className="mr-6 md:mr-8 w-1/3 h-auto">
+      <div className="w-20 h-20 md:w-40 md:h-40 md:mx-auto">
         <Image
           src={
             "https://res.cloudinary.com/dpkreativ/image/upload/v1616357794/IMG_-oi62yk_wtfop2.jpg" ||
             null
           }
+          className="rounded-full"
+          width="100%"
+          height="100%"
+          alt="main profile"
         />
       </div>
     </div>
