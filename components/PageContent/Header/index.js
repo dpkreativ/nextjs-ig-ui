94820912d1290d6485f0cedc58d25283573334f8
Divine Orji <dpkreativ@gmail.com>
2021-09-05 23:31:05 +0100

    add profile user data component

--- components/PageContent/Header/index.js
+++ components/PageContent/Header/index.js
@@ -1,9 +1,11 @@
 import ProfileImage from "./ProfileImage";
+import ProfileUserData from "./ProfileUserData";
 
 const Header = () => {
   return (
     <header className="p-5 flex">
       <ProfileImage />
+      <ProfileUserData />
     </header>
   );
 };
