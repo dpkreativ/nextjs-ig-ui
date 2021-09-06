6fa145f49b6b6130faf69f9751ce0d067d38e7b0
Divine Orji <dpkreativ@gmail.com>
2021-09-06 09:23:46 +0100

    create Bio component

---
+++ components/PageContent/GeneralContent/Bio.js
@@ -0,0 +1,14 @@
+const Bio = () => {
+  return (
+    <div className="text-sm p-5 md:hidden">
+      <h1 className="font-bold">Estelle</h1>
+      <p>
+        An online store 🏬 we deal on all kinds of footwear 👠👞 for both male
+        and female. Contact us ☎ @08141703050. Worldwide delivery 🌍 @Owerri,
+        Nigeria
+      </p>
+    </div>
+  );
+};
+
+export default Bio;
