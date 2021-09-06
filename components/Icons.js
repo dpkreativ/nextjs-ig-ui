c40f72310d452cec96dcc5228d0978051f58009e
Divine Orji <dpkreativ@gmail.com>
2021-09-06 07:28:09 +0100

    add chevron icon

--- components/Icons.js
+++ components/Icons.js
@@ -101,3 +101,19 @@ export function OptionsIcon({ className }) {
     </svg>
   );
 }
+
+export function DownChevronIcon({ className }) {
+  return (
+    <svg
+      aria-label="Down Chevron Icon"
+      class={className}
+      fill={"currentColor" || "#262626"}
+      height="12"
+      role="img"
+      viewBox="0 0 48 48"
+      width="12"
+    >
+      <path d="M40 33.5c-.4 0-.8-.1-1.1-.4L24 18.1l-14.9 15c-.6.6-1.5.6-2.1 0s-.6-1.5 0-2.1l16-16c.6-.6 1.5-.6 2.1 0l16 16c.6.6.6 1.5 0 2.1-.3.3-.7.4-1.1.4z"></path>
+    </svg>
+  );
+}
