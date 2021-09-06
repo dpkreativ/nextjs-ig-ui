3ff05f53c0f2bf8e779d3f91ee7e8955541acdc6
Divine Orji <dpkreativ@gmail.com>
2021-09-06 09:17:52 +0100

    add max-width and margin-auto

--- components/PageContent/Header/index.js
+++ components/PageContent/Header/index.js
@@ -3,7 +3,7 @@ import ProfileUserData from "./ProfileUserData";
 
 const Header = () => {
   return (
-    <header className="p-5 flex">
+    <header className="p-5 flex max-w-screen-lg mx-auto">
       <ProfileImage />
       <ProfileUserData />
     </header>
