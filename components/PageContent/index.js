f204755f50bf6e848de63ae7091dd077cd9fa62e
Divine Orji <dpkreativ@gmail.com>
2021-09-06 09:49:35 +0100

    import highlights

--- components/PageContent/index.js
+++ components/PageContent/index.js
@@ -1,12 +1,14 @@
 import Bio from "./GeneralContent/Bio";
 import Numbers from "./GeneralContent/Numbers";
 import Header from "./Header";
+import Highlights from "./Highlights";
 
 const PageContent = () => {
   return (
     <main>
       <Header />
       <Bio className="p-5 md:hidden" />
+      <Highlights />
       <Numbers className="grid grid-cols-3 text-center border-t border-b border-gray-200 py-4 md:hidden" />
     </main>
   );
