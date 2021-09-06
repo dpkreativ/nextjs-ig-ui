b9d983b4f173e9a6fbdaeb99d9e55843d43a3c22
Divine Orji <dpkreativ@gmail.com>
2021-09-06 07:19:12 +0100

    add options icon

--- components/Icons.js
+++ components/Icons.js
@@ -65,3 +65,39 @@ export function HeartIcon({ className }) {
     </svg>
   );
 }
+
+export function OptionsIcon({ className }) {
+  return (
+    <svg
+      aria-label="Options"
+      class={className}
+      fill={"currentColor" || "#262626"}
+      height="24"
+      role="img"
+      viewBox="0 0 48 48"
+      width="24"
+    >
+      <circle
+        clipRule="evenodd"
+        cx="8"
+        cy="24"
+        fillRule="evenodd"
+        r="4.5"
+      ></circle>
+      <circle
+        clipRule="evenodd"
+        cx="24"
+        cy="24"
+        fillRule="evenodd"
+        r="4.5"
+      ></circle>
+      <circle
+        clipRule="evenodd"
+        cx="40"
+        cy="24"
+        fillRule="evenodd"
+        r="4.5"
+      ></circle>
+    </svg>
+  );
+}
