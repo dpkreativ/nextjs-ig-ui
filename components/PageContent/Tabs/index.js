c6a841a291d0748cc707050bc7ba118ee3e4e032
Divine Orji <dpkreativ@gmail.com>
2021-09-06 11:15:10 +0100

    tweak tab Ui

--- components/PageContent/Tabs/index.js
+++ components/PageContent/Tabs/index.js
@@ -3,10 +3,14 @@ import Posts from "./Posts";
 
 const Tabs = () => {
   return (
-    <section>
-      <div className="p-5 flex justify-center">
-        <button type="button" className="text-blue-400">
-          <PostsIcon />
+    <section className="max-w-screen-lg mx-auto px-5">
+      <div className="p-5 flex justify-center md:border-t border-gray-300">
+        <button
+          type="button"
+          className="text-blue-400 md:text-black flex items-center md:text-sm"
+        >
+          <PostsIcon className="md:h-4" />
+          <span className="hidden md:block uppercase">Posts</span>
         </button>
       </div>
       <div>
