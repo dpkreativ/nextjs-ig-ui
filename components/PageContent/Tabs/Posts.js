8f2778df776f166376c27e89fdeab03bad06ce87
Divine Orji <dpkreativ@gmail.com>
2021-09-06 11:15:22 +0100

    tweak posts UI

--- components/PageContent/Tabs/Posts.js
+++ components/PageContent/Tabs/Posts.js
@@ -1,6 +1,6 @@
 const Posts = () => {
   return (
-    <div className="grid grid-cols-3 gap-1">
+    <div className="grid grid-cols-3 gap-1 md:gap-8">
       <div className="bg-gray-400 aspect-w-1 aspect-h-1"></div>
       <div className="bg-gray-400 aspect-w-1 aspect-h-1"></div>
       <div className="bg-gray-400 aspect-w-1 aspect-h-1"></div>
