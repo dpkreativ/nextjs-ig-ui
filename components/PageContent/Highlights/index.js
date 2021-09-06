b6cc06da267463d2a28682f3805002069365c4ff
Divine Orji <dpkreativ@gmail.com>
2021-09-06 10:04:49 +0100

    add content to highlights component

--- components/PageContent/Highlights/index.js
+++ components/PageContent/Highlights/index.js
@@ -1,9 +1,22 @@
 const Highlights = () => {
   return (
-    <div>
-      <h1>Highlights</h1>
-      These will be the highlights
-    </div>
+    <section
+      id="highlights"
+      className="p-5 flex max-w-screen-lg mx-auto space-x-3 md:space-x-10"
+    >
+      <div className="border border-gray-300 rounded-full p-0.5">
+        <div className="h-14 w-14 md:h-20 md:w-20 rounded-full bg-gray-100"></div>
+      </div>
+      <div className="border border-gray-300 rounded-full p-0.5">
+        <div className="h-14 w-14 md:h-20 md:w-20 rounded-full bg-gray-100"></div>
+      </div>
+      <div className="border border-gray-300 rounded-full p-0.5">
+        <div className="h-14 w-14 md:h-20 md:w-20 rounded-full bg-gray-100"></div>
+      </div>
+      <div className="border border-gray-300 rounded-full p-0.5">
+        <div className="h-14 w-14 md:h-20 md:w-20 rounded-full bg-gray-100"></div>
+      </div>
+    </section>
   );
 };
 
