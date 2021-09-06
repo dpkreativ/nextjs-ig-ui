2903405bac014fcc6f8922543cda518671889dbc
Divine Orji <dpkreativ@gmail.com>
2021-09-06 10:42:40 +0100

    import tabs component

--- components/PageContent/index.js
+++ components/PageContent/index.js
@@ -2,6 +2,7 @@ import Bio from "./GeneralContent/Bio";
 import Numbers from "./GeneralContent/Numbers";
 import Header from "./Header";
 import Highlights from "./Highlights";
+import Tabs from "./Tabs";
 
 const PageContent = () => {
   return (
@@ -10,6 +11,7 @@ const PageContent = () => {
       <Bio className="p-5 md:hidden" />
       <Highlights />
       <Numbers className="grid grid-cols-3 text-center border-t border-b border-gray-200 py-4 md:hidden" />
+      <Tabs />
     </main>
   );
 };
