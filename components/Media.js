621ccabc58fa7584eff5eac4e20cb675cf34db26
Divine Orji <dpkreativ@gmail.com>
2021-09-07 21:27:19 +0100

    add a tag to each object

--- components/Media.js
+++ components/Media.js
@@ -1,18 +1,22 @@
 export const Media = [
   {
     id: 1,
+    tag: "img",
     url: "https://res.cloudinary.com/dpkreativ/image/upload/v1631011637/nextjs-ig-ui/img_8_ehjxmc.jpg",
   },
   {
     id: 2,
+    tag: "vid",
     url: "https://res.cloudinary.com/dpkreativ/image/upload/v1631011509/nextjs-ig-ui/img_9_zpwfqg.jpg",
   },
   {
     id: 3,
+    tag: "img",
     url: "https://res.cloudinary.com/dpkreativ/image/upload/v1631011615/nextjs-ig-ui/img_5_pows0n.jpg",
   },
   {
     id: 4,
+    tag: "img",
     url: "https://res.cloudinary.com/dpkreativ/image/upload/v1631011574/nextjs-ig-ui/img_7_iqkmm8.jpg",
   },
   {
@@ -21,18 +25,22 @@ export const Media = [
   },
   {
     id: 6,
+    tag: "img",
     url: "https://res.cloudinary.com/dpkreativ/image/upload/v1631011514/nextjs-ig-ui/img_3_fms9ww.jpg",
   },
   {
     id: 7,
+    tag: "img",
     url: "https://res.cloudinary.com/dpkreativ/image/upload/v1631011500/nextjs-ig-ui/img_12_jvdruv.jpg",
   },
   {
     id: 8,
+    tag: "img",
     url: "https://res.cloudinary.com/dpkreativ/image/upload/v1631011499/nextjs-ig-ui/img_10_aj638t.jpg",
   },
   {
     id: 9,
+    tag: "img",
     url: "https://res.cloudinary.com/dpkreativ/image/upload/v1631011498/nextjs-ig-ui/img_11_xp9lxn.jpg",
   },
 ];
