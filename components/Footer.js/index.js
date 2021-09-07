06dd4df7b53a51f68be2bfedefb63dd8ca8a5029
Divine Orji <dpkreativ@gmail.com>
2021-09-07 08:20:28 +0100

    add content to footer component

--- components/Footer.js/index.js
+++ components/Footer.js/index.js
@@ -1,8 +1,27 @@
 const Footer = () => {
   return (
-    <footer>
-      <div>Top</div>
-      <div>Bottom</div>
+    <footer className="max-w-screen-lg mx-auto pt-5 px-5 md:pt-12 pb-12 flex flex-col items-center text-xs">
+      <div className="flex flex-wrap space-x-3 justify-center">
+        <p>About</p>
+        <p>Blog</p>
+        <p>Jobs</p>
+        <p>Help</p>
+        <p>API</p>
+        <p>Privacy</p>
+        <p>Terms</p>
+        <p>Top Accounts</p>
+        <p>Hashtags</p>
+        <p>Locations</p>
+        <p>Instagram Lite</p>
+      </div>
+      <div className="flex space-x-3 py-5">
+        <select name="languages" id="languages" className="outline-none">
+          <option value="English">English</option>
+          <option value="France">France</option>
+          <option value="Italian">Italian</option>
+        </select>
+        <div>&#169; 2021 Instagram from Facebook</div>
+      </div>
     </footer>
   );
 };
