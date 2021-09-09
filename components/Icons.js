186d52ea574ae7c6921a01b0d1084d23ebc9c8d2
Divine Orji <dpkreativ@gmail.com>
2021-09-09 13:29:25 +0100

    Update Icons.js

--- components/Icons.js
+++ components/Icons.js
@@ -154,3 +154,20 @@ export function PostsIcon({ className }) {
     </svg>
   );
 }
+
+export function CloseIcon({ className }) {
+  return (
+    <svg
+      aria-label="Close"
+      className={className}
+      fill={"currentColor" || "#5a5a5a"}
+      role="button"
+      viewBox="0 0 24 24"
+      width="24"
+      height="24"
+    >
+      <path fill="none" d="M0 0h24v24H0z" />
+      <path d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z" />
+    </svg>
+  );
+}
