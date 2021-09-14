e8439ea7c687192678899f681348455faded080f
Divine Orji <dpkreativ@gmail.com>
2021-09-14 10:16:54 +0100

    add modal content

--- components/Modal.js
+++ components/Modal.js
@@ -1,16 +1,25 @@
 import { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 
-const Modal = () => {
+const Modal = ({ show, children }) => {
   const [isBrowser, setIsBrowser] = useState(false);
 
   useEffect(() => {
     setIsBrowser(true);
   }, []);
 
+  const modalContent = show ? (
+    <div
+      id="modal-backdrop"
+      className="fixed inset-0 bg-gray-600 flex justify-between items-center p-5"
+    >
+      {children}
+    </div>
+  ) : null;
+
   if (isBrowser) {
     return ReactDOM.createPortal(
-      <div>Hello, I am modal</div>,
+      modalContent,
       document.getElementById("modal-root")
     );
   } else {
